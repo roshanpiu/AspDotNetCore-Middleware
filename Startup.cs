019504cc7cf08dbfbd547a8a7367a1cf40415fe6
Roshan Piumal <RoshanP@mitrai.local>
2017-02-14 16:00:30 +0530

    added request timer middleware

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,18 @@ namespace WebApplication
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            
+            loggerFactory.AddConsole();
+
+            app.Use(next => async context =>
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+                await next(context);
+                timer.Stop();
+                var logger = loggerFactory.CreateLogger("Request Timer");
+                logger.LogInformation($"Request to {context.Request.Path} took {timer.ElapsedMilliseconds} ms");
+            });
+
             app.Run(async (context) => 
                 await context.Response.WriteAsync("Hello World")
             );
